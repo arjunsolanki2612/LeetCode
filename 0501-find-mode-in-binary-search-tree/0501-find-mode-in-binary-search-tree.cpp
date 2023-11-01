631a61b217ef9e9da02e7105d991465548ba7f40
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2023-11-01 23:27:27 +0530

    Update 0501-find-mode-in-binary-search-tree.cpp

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -37,3 +37,52 @@ unordered_map<int, int>mp;
         return ans;
     }
 };
+
+// second apporac without extra space
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        int currEle = 0;
+        int currFreq = 0;
+        int maxFreq = 0;
+    void inorder(TreeNode* root,vector<int>&ans){
+        if(root!=NULL){
+            inorder(root->left,ans);
+            // kya current element root ki value k barabar h
+            if(root->val==currEle){
+                currFreq++;
+            }else{
+                currEle = root->val;
+                currFreq=1;
+            }
+            // kya current freq max freq se badi h to hume koi naya number mila h
+            if(currFreq>maxFreq){
+                ans={};
+                maxFreq=currFreq;
+
+            }
+            if(currFreq==maxFreq){
+                ans.push_back(currEle);
+            }
+            inorder(root->right,ans);
+        }
+    }
+    vector<int> findMode(TreeNode* root) {
+        vector<int>ans;
+        
+        inorder(root,ans);
+        return ans;
+
+        
+    }
+};
