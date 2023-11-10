52fd05b0c902beac16358720728485a1e6b753cd
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2023-11-10 23:45:40 +0530

    Update 1866-restore-the-array-from-adjacent-pairs.cpp

--- 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-unordered_map<int, vector<int>> ps;
-    for (auto &p : adjacentPairs) {
-        ps[p[0]].push_back(p[1]);
-        ps[p[1]].push_back(p[0]);
-    }
-    vector<int> res;
-    for (auto &p : ps) {
-        if (p.second.size() == 1) {
-            res.push_back(p.first);
-            res.push_back(p.second[0]);
-            break;
+
+    vector<int>res;
+    void dfs(int u, int prev, unordered_map<int,vector<int>>&mp){
+        res.push_back(u);
+        for(auto &v:mp[u]){
+            if(v!=prev){
+                dfs(v,u,mp);
+            }
         }
     }
-    while (res.size() < adjacentPairs.size() + 1) {
-        auto tail = res.back(), prev = res[res.size() - 2];
-        auto &next = ps[tail];
-        if (next[0] != prev)
-            res.push_back(next[0]);
-        else
-            res.push_back(next[1]);
-    }
-    return res;
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int,vector<int>>mp;
+        for(vector<int> vec: adjacentPairs){
+            int u = vec[0];
+            int v = vec[1];
+
+            mp[u].push_back(v);
+            mp[v].push_back(u);
+        }
+        int startPoint=-1;
+        for(auto &it:mp){
+            if(it.second.size()==1){
+                startPoint = it.first;
+                break;
+            }
+        }
+
+        dfs(startPoint,INT_MIN,mp); //(startPoint, prev);
+        return res;
     }
 };
