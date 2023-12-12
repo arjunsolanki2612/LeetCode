6855e861f6cb9917f0d9e32ea60c1e544b6dfcc6
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2023-12-12 17:12:30 +0530

    Create Maximum Product of Two Elements in an Array.cpp

---
+++ Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,36 @@
+/*
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+Example 1:
+
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+Example 2:
+
+Input: nums = [1,5,4,5]
+Output: 16
+Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+Example 3:
+
+Input: nums = [3,7]
+Output: 12
+*/
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int first_max = -1;
+        int second_max = -1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=first_max){
+                second_max = first_max;
+                first_max = nums[i];
+            }else{
+                second_max = max(second_max,nums[i]);
+            }
+        }
+
+        return (second_max-1)*(first_max-1);
+    }
+};
