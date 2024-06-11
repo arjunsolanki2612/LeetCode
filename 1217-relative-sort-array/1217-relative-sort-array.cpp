e9b01b6f0c72de4c48ae850448aaabe9e0d1a4a6
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2024-06-11 10:45:51 +0530

    Update 1217-relative-sort-array.cpp

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,3 +1,35 @@
+// Approach 1 : Counting sort
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int,int>mp;
+        vector<int>ans;
+        for(auto &num:arr1){
+            mp[num]++;
+        }
+
+        for(int i=0;i<arr2.size();i++){
+            if(mp.find(arr2[i])!=mp.end()){
+                int freq = mp[arr2[i]];
+                for(int j=0;j<freq;j++){
+                    ans.push_back(arr2[i]);
+                }
+                mp[arr2[i]]=0;
+            }
+        }
+        for(auto &x:mp){
+            if(x.second>0){
+                int freq = x.second;
+                for(int i=0;i<freq;i++){
+                    ans.push_back(x.first);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//Approach 2 : Custom sort using Lambda function
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
