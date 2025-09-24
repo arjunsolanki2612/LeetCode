353dcd0c7325e5eb2774488d6a10a61c75e54633
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2025-09-24 11:30:25 +0530

    Time: 2 ms (18.54%), Space: 9.4 MB (8.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int find(int i , int j , int m , int n, vector<vector<int>>&dp){
-        if(i>=m || j >= n)return 0 ;
-        if(i == m-1 && j == n-1) return 1 ;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+    vector<vector<int>>directions = {{1,0},{0,1}};
+    
+    int path(int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1){
+            return 1;
         }
-        dp[i][j] =  find(i+1,j,m,n,dp)+find(i,j+1,m,n,dp);
-        return dp[i][j];
+        if(i<0 || j<0 || i>=m || j>=n){
+            return 0;
+        }
+        int totalPath = 0;
+        if(dp[i][j]!=-1){
+             return dp[i][j];
+        }
+
+       
+        for(auto &dir : directions){
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+
+            totalPath += path(new_i,new_j,m,n,dp);
+        }
+        return dp[i][j] = totalPath;
     }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return find(0,0,m,n,dp);
+
+     int uniquePaths(int m, int n) {
+        vector<vector<int>>dp (m,vector<int>(n,-1));
+        return path(0,0,m,n,dp);
+        
     }
 };
