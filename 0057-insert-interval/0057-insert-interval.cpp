fd87882799cb4046e69866d1982d865f3042ec15
Arjun Solanki <92087686+arjunsolanki2612@users.noreply.github.com>
2025-11-08 00:01:36 +0530

    Time: 0 ms (100%), Space: 21.7 MB (46.16%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,34 +2,33 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        vector<vector<int>>arrItval;
-        bool isInsert = false;
-        for(auto x: intervals){
-            if(x[0]<newInterval[0]){
-                arrItval.push_back(x);
-            }else{
-                if(isInsert==false){
-                    arrItval.push_back(newInterval);
-                    isInsert = true;
-                }
-
-                arrItval.push_back(x); 
-            }
-        }
-        if(!isInsert){
-            arrItval.push_back(newInterval);
-        }
         vector<vector<int>>mergeInterval;
-        mergeInterval.push_back(arrItval[0]);
-        for(int i=1;i<arrItval.size();i++){
-            auto &last = mergeInterval.back();
-            if(arrItval[i][0]<=last[1]){
-                last[1] = max(last[1],arrItval[i][1]);
-            }else{
-                mergeInterval.push_back(arrItval[i]);
-            }
+        int i = 0, n = intervals.size();
+        //merge all inteval that come before newInterval
+        while(i<n && intervals[i][1]<newInterval[0]){
+            mergeInterval.push_back(intervals[i]);
+            i++;
+        }
+        //merge intervals with newInterval that overlap
+        //-- newInterval -->[4,8]
+        //-- remaining elements in intervals --> [3,5],[6,7],[8,10],[12,16]
+        //to check whether they will overlap, we see if intervals[i][0]<=newInterval[1]
+
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
+            i++;
         }
 
+        mergeInterval.push_back(newInterval);
+
+        //now add all non overlapping intervals
+
+        while(i<n){
+            mergeInterval.push_back(intervals[i]);
+            i++;
+        }
+        
         return mergeInterval;
     }
 };
